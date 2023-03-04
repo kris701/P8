7d72205eeb48779af939360f7607f52181f612d4
NicholasHDK <nichherr@gmail.com>
2023-03-04 17:32:25 +0100

    added data writer

--- protonets/data_writer.py
+++ protonets/data_writer.py
@@ -1,5 +1,5 @@
+import os
 import pandas as pd
-FP = "path to swedish leaf data set"
 
 def load_and_format_data(fp):
     df = pd.read_csv(fp, delimiter="\t", header=None)
@@ -10,4 +10,9 @@ def load_and_format_data(fp):
         classes.append([X.iloc[i] for i in range(len(y)) if y[i] == c])
     return classes
 
-
+def write_data(classes, destination_folder):
+    os.makedirs(destination_folder)
+    for j, c in enumerate(classes):
+        os.makedirs(destination_folder + "\\" + str(j))
+        for i, item in enumerate(c):
+            item.to_csv(path_or_buf=destination_folder + "\\" + str(j) + "\\" + str(i) + ".csv", sep=',', header=False)
