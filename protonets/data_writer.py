c1532228ba62e93caf824fe6436d1b0f4223929b
NicholasHDK <nichherr@gmail.com>
2023-03-05 13:43:44 +0100

    Fixed a bug

--- protonets/data_writer.py
+++ protonets/data_writer.py
@@ -1,18 +1,23 @@
 import os
 import pandas as pd
 
+#some paths you might find helpful:
+TARGET_FP = "C:\\Users\\test\\Documents\\GitHub\\P8\\protonets\\prototypical-networks-master\\data\\SwedishLeaf\\data"
+SRC_FP = "C:\\Users\\test\\Documents\\Data\\UCRArchive_2018\\SwedishLeaf\\SwedishLeaf_TRAIN.tsv"
+
+
 def load_and_format_data(fp):
     df = pd.read_csv(fp, delimiter="\t", header=None)
     X = df.iloc[:, 1:]
     y = df.iloc[:, 0]
     classes = []
-    for c in range(len(set(y))):
+    for c in set(y):
         classes.append([X.iloc[i] for i in range(len(y)) if y[i] == c])
     return classes
 
 def write_data(classes, destination_folder):
     os.makedirs(destination_folder)
     for j, c in enumerate(classes):
-        os.makedirs(destination_folder + "\\" + str(j))
+        os.makedirs(destination_folder + "\\" + str(j+1))
         for i, item in enumerate(c):
-            item.to_csv(path_or_buf=destination_folder + "\\" + str(j) + "\\" + str(i) + ".csv", sep=',', header=False)
+            item.to_csv(path_or_buf=destination_folder + "\\" + str(j+1) + "\\" + str(i) + ".csv", sep=',', header=False, index=False)
