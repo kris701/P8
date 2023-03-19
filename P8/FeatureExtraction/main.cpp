1962c0822d81941adc36f28d14488738b5e43cc8
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-03-19 15:22:37 +0100

    Adds feature extraction

---
+++ P8/FeatureExtraction/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include "cxxopts/cxxopts.hpp"
+#include "src/FileHanding.h"
+#include "src/FeatureFinding.h"
+#include "src/SeriesUtils.h"
+#include "src/Logger.h"
+
+struct Arguments {
+    std::string trainPath;
+    std::string testPath;
+    std::optional<double> split;
+    std::optional<uint> minWindowSize;
+    std::optional<uint> maxWindowSize;
+};
+
+Arguments ParseArguments (int argc, char **argv) {
+    cxxopts::Options options("FeatureExtraction", "Extracts features from given files");
+    options.add_options()
+            ("train", "Required - Path to train data (Absolute)", cxxopts::value<std::string>())
+            ("test", "Required - Path to test data (Absolute)", cxxopts::value<std::string>())
+            ("split", "Optional - How much of the data should be training data (0,1)", cxxopts::value<double>())
+            ("h,help", "Print usage")
+            ;
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+    Arguments arguments;
+    if (!result.count("train")) {
+        std::cout << "Missing train path." << std::endl;
+        exit(1);
+    } else {
+        arguments.trainPath = result["train"].as<std::string>();
+    }
+
+    if (!result.count("test")) {
+        std::cout << "Missing test path." << std::endl;
+        exit(1);
+    } else {
+        arguments.testPath = result["test"].as<std::string>();
+    }
+
+    if (result.count("split")) {
+        const double split = result["split"].as<double>();
+        if (split < 0 || split > 1) {
+            std::cout << "Split must be between 0 and 1." << std::endl;
+            exit(1);
+        } else if (split == 0) {
+            std::cout << "Split must be over 0, as it is impossible to create features from noting." << std::endl;
+            exit(1);
+        } else {
+            arguments.split = split;
+        }
+    }
+
+    return arguments;
+}
+
+int main(int argc, char **argv) {
+    uint id = Logger::Begin("Parsing Arguments");
+    auto arguments = ParseArguments(argc, argv);
+    Logger::End(id);
+
+    id = Logger::Begin("Reading Data");
+    const auto tempTrainData = FileHanding::ReadCSV(arguments.trainPath, "\t");
+    const auto tempTestData = FileHanding::ReadCSV(arguments.testPath, "\t");
+    Logger::End(id);
+
+    id = Logger::Begin("Generating Data based on Split.");
+    if (!arguments.split.has_value())
+        arguments.split = (double) tempTrainData.size() / (double) tempTestData.size();
+
+    std::vector<LabelledSeries> combinedData;
+    for (const auto &seriesSet : { tempTrainData, tempTestData })
+        for (const auto &s : seriesSet)
+            combinedData.push_back(s);
+    std::vector<LabelledSeries> trainData;
+    const uint trainCount = (uint) std::round((double) combinedData.size() * arguments.split.value());
+    for (uint i = 0; i < trainCount; i++)
+        trainData.push_back(combinedData.at(i));
+    Logger::End(id);
+
+
+    id = Logger::Begin("Generating Feature Set");
+    const auto featureSet = FeatureFinding::GenerateFeatureTree(3, trainData, SeriesUtils::GetCount(trainData));
+    Logger::End(id);
+
+    id = Logger::Begin("Generating Data Map");
+    std::unordered_map<int, std::vector<Series>> mappedData;
+    for (const auto &d : combinedData)
+        mappedData[d.label].push_back(d.series);
+    Logger::End(id);
+
+    id = Logger::Begin("Writing to Files");
+    if (std::filesystem::is_directory("out"))
+        std::filesystem::remove_all("out");
+    for (const auto &seriesSet : mappedData) {
+        const std::string dirPath = "out/data/" + std::to_string(seriesSet.first) + "/";
+        for (uint i = 0; i < seriesSet.second.size(); i++) {
+            const std::string filePath = dirPath + std::to_string(i);
+            const auto featureSeries = FeatureFinding::GenerateFeatureSeries(seriesSet.second.at(i), featureSet);
+            FileHanding::WriteFile(filePath, featureSeries);
+        }
+    }
+    Logger::End(id);
+
+    return 0;
+}
---
+++ P8/FeatureExtraction/src/FeatureFinding.h
@@ -0,0 +1,138 @@
+#ifndef FEATUREEXTRACTION_FEATUREFINDING_H
+#define FEATUREEXTRACTION_FEATUREFINDING_H
+
+#include <stdexcept>
+#include "Types.h"
+#include "SeriesUtils.h"
+#include "InformationGain.h"
+#include "Attributes.h"
+#include "WindowGeneration.h"
+#include "Logger.h"
+#include "indicators/cursor_control.hpp"
+#include "indicators/block_progress_bar.hpp"
+
+namespace FeatureFinding {
+    [[nodiscard]] double EvaluateWindow(double priorEntropy, double bestScore, const ClassCount &counts, Attribute attribute,
+                                        const std::vector<LabelledSeries> &series, const Series &window) {
+        std::map<double, ClassCount> valueCount; // At the given value, how many of each class
+
+        ClassCount diff { counts };
+        for (const auto &s : series) {
+            valueCount[Attributes::GenerateValue(s.series, window, attribute)][s.label]++;
+            diff.at(s.label)--;
+
+            if (valueCount.size() < 2) // No split point at single value
+                continue;
+
+            // Early entropy pruning
+            const double optimalSplitPoint = InformationGain::GetOptimalSplitPoint(valueCount);
+            const auto split = InformationGain::GetSplit(valueCount, optimalSplitPoint);
+            for (int i = 0; i < MAX_CLASSES; i++) // Adds the rest of the values optimally
+                valueCount[split.first[i] > split.second[i]][i] += diff[i];
+
+            const double optimalGain = InformationGain::CalculateInformationGain(valueCount, priorEntropy);
+            if (optimalGain < bestScore)
+                return 0;
+            for (int i = 0; i < MAX_CLASSES; i++)
+                valueCount[split.first[i] > split.second[i]][i] -= diff[i];
+        }
+
+        return InformationGain::CalculateInformationGain(valueCount, priorEntropy);
+    }
+
+    [[nodiscard]] Feature FindOptimalFeature(const std::vector<LabelledSeries> &series, const std::vector<Series> &windows) {
+        if (series.size() < 2)
+            throw std::logic_error("Cannot find features for less than two series.");
+        if (windows.empty())
+            throw std::logic_error("Missing windows.");
+
+        const ClassCount counts = SeriesUtils::GetCount(series);
+        const double currentEntropy = InformationGain::CalculateEntropy(counts);
+
+        std::optional<Series> optimalShapelet;
+        std::optional<Attribute> optimalAttribute;
+        double optimalGain = 0;
+
+        indicators::show_console_cursor(false);
+        using namespace indicators;
+        BlockProgressBar bar{
+                option::BarWidth{80},
+                option::ForegroundColor{Color::white},
+                option::FontStyles{
+                    std::vector<FontStyle>{FontStyle::bold}},
+                option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
+                option::MaxProgress{windows.size()}
+        };
+
+        for (uint i = 0; i < windows.size(); i++) {
+            const auto& window = windows.at(i);
+            for (const auto &attribute: ATTRIBUTES) {
+                const double gain = EvaluateWindow(currentEntropy, optimalGain, counts, attribute, series, window);
+
+                if (!optimalShapelet.has_value() || gain > optimalGain) {
+                    optimalShapelet = window;
+                    optimalAttribute = attribute;
+                    optimalGain = gain;
+                }
+            }
+            bar.set_option(option::PostfixText{
+                    std::to_string(i) + "/" + std::to_string(windows.size())
+            });
+            bar.tick();
+        }
+        indicators::show_console_cursor(true);
+        bar.mark_as_completed();
+
+
+        return Feature(optimalShapelet.value(), optimalAttribute.value());
+    }
+
+    // *Not actually a tree
+    [[nodiscard]] std::vector<Feature> GenerateFeatureTree(int depth, const std::vector<LabelledSeries> &series, const ClassCount &counts) {
+        std::vector<Feature> features;
+        if (depth == 0)
+            return features;
+
+        uint featureId = Logger::Begin("Generating Feature for Depth " + std::to_string(depth));
+
+        const auto windows = WindowGeneration::GenerateWindows(series, 2, 8);
+        const auto feature = FindOptimalFeature(series, windows);
+        features.push_back(feature);
+
+        uint splitId = Logger::Begin("Retriving optimal split");
+        const double splitPoint = InformationGain::GetOptimalSplitPoint(Attributes::GenerateValues(series, feature.shapelet, feature.attribute));
+        const auto split = Attributes::SplitSeries(series, feature.attribute, feature.shapelet, splitPoint);
+        Logger::End(splitId);
+
+        Logger::End(featureId);
+
+        for (const auto &s : { split.below, split.above }) {
+            const auto tempCounts = SeriesUtils::GetCount(s);
+
+            bool harmonious = true; // All elements in either side of split, or it is empty
+            for (uint i = 0; i < MAX_CLASSES; i++)
+                if (tempCounts[i] != 0 && tempCounts[i] != counts[i]) {
+                    harmonious = false;
+                    break;
+                }
+            if (harmonious)
+                continue;
+
+            for (const auto &f: GenerateFeatureTree(depth - 1, s, tempCounts))
+                features.push_back(f);
+        }
+
+        return features;
+    }
+
+    std::vector<double> GenerateFeatureSeries(const Series &series, const std::vector<Feature> &features) {
+        std::vector<double> featureSeries;
+
+        for (const auto &feature : features)
+            featureSeries.push_back(Attributes::GenerateValue(series, feature.shapelet, feature.attribute));
+
+        return featureSeries;
+    }
+}
+
+#endif //FEATUREEXTRACTION_FEATUREFINDING_H
