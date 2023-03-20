d273ec9eaae7a12b9eb53a366bab9436939acf7e
Kristian Skov <kris701kj@gmail.com>
2023-03-20 11:39:00 +0100

    Fixed syntax error

--- P8/DataConverters/DirectDataConverter/__init__.py
+++ P8/DataConverters/DirectDataConverter/__init__.py
@@ -72,11 +72,11 @@ class DirectDataConverter(BaseDataConverter):
         return [train_data[i] + test_data[i] for i in range(len(train_data))]
 
     def ConvertData(self):
-        if not os.path.isdir(self.TargetDir):
+        if not os.path.isdir(self.Options.FormatedFolder):
             print("Formating dataset. This may take a while...")
             formated = self._formatData(self.Options.SourceTrainData, self.Options.SourceTestData)
-            self._write_data(formated, self.TargetDir)
-            self._create_splits(self.TargetDir, int(len(formated) * (1 - self.Options.TestClassesSplit)), self.Options.TrainValSplit)
+            self._write_data(formated, self.Options.FormatedFolder)
+            self._create_splits(self.Options.FormatedFolder, int(len(formated) * (1 - self.Options.TestClassesSplit)), self.Options.TrainValSplit)
             print("Formating complete!")
         else:
             print("Dataset already formated!")
