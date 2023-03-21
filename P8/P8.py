d399fa056948a479206b729968919c3af45f1ab8
Kristian Skov <kris701kj@gmail.com>
2023-03-21 12:38:46 +0100

    Set default values for the feature extractor

--- P8/P8.py
+++ P8/P8.py
@@ -18,6 +18,8 @@ dataOptions.TrainValSplit = 0.7;
 dataOptions.TestClassesSplit = 0.2;
 dataOptions.SourceTrainData = "./Data/SwedishLeaf/SwedishLeaf_TRAIN.tsv";
 dataOptions.SourceTestData = "./Data/SwedishLeaf/SwedishLeaf_TEST.tsv";
+dataOptions.depth = 3;
+dataOptions.maxWindowSize = 8;
 
 #dataConverter = DirectDataConverter(dataOptions);
 dataConverter = ShapeletHistogramConverter(dataOptions);
