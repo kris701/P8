61baf8cb0b964f72237f7be3d39c1a0d7fdf1585
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-04-02 17:22:42 +0200

    Adds more experiments

--- P8/P8.py
+++ P8/P8.py
@@ -2,12 +2,15 @@ from ExperimentSuite import ExperimentSuite
 from DataVisualisers.ShapeletHistogramVisualiser import ShapeletHistogramVisualiser
 
 #experiments = ["SwedishLeaf", "Beef", "CricketX", "ElectricDevices", "Wine"]
-experiments = ["SwedishLeaf"]
+experiments = ["Adiac", "Beef", "BeetleFly", "BirdChicken", "Coffee", "CricketX", "ECH200", "ElectricDevices", "FaceAll",
+               "FaceFour", "FordA", "FordB", "MedicalImages", "StrawBerry", "SwedishLeaf", "SyntheticControl", "TwoPatterns",
+               "Wine", "Yoga"]
+#experiments = ["SwedishLeaf"]
 
 expSuite = ExperimentSuite(experiments)
 expSuite.RunExperiments()
 
-print("Visualizing swedish leafs")
-visualizer = ShapeletHistogramVisualiser(".\\Formatted\\SwedishLeaf");
-visualizer.VisualizeAllClasses();
-visualizer.VisualizeClass(1);
+#print("Visualizing swedish leafs")
+#visualizer = ShapeletHistogramVisualiser(".\\Formatted\\SwedishLeaf");
+#visualizer.VisualizeAllClasses();
+#visualizer.VisualizeClass(1);
