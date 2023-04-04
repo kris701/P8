d7b1b81cc8efe17e891fe927f67af57d73068a10
Kristian Skov <kris701kj@gmail.com>
2023-04-04 10:25:01 +0200

    Returned the main p8 file

--- P8/P8.py
+++ P8/P8.py
@@ -3,7 +3,10 @@ from DataVisualisers.ShapeletHistogramVisualiser import ShapeletHistogramVisuali
 from DataVisualisers.ResultsVisualiser import ResultsVisualiser
 from ResultsCombiners.CSVResultsCombiner import CSVResultsCombiner
 
-experiments = ["SwedishLeaf", "Beef", "CricketX", "ElectricDevices", "Wine"]
+experiments = ["Adiac", "Beef", "BeetleFly", "BirdChicken", "Coffee", "CricketX", "ECH200", "ElectricDevices", "FaceAll",
+               "FaceFour", "FordA", "FordB", "MedicalImages", "StrawBerry", "SwedishLeaf", "SyntheticControl", "TwoPatterns",
+               "Wine", "Yoga"]
+#experiments = ["SwedishLeaf", "Beef", "CricketX", "ElectricDevices", "Wine"]
 #experiments = ["SwedishLeaf"]
 
 expSuite = ExperimentSuite(experiments, "Ours")
