8b44f736a392891d108b03e382939aa4f10e0307
Kristian Skov <kris701kj@gmail.com>
2023-04-04 13:49:23 +0200

    Added the new comparsion data and all its configs

--- ComparisonData/readme.md
+++ ComparisonData/readme.md
@@ -1,3 +1,4 @@
 # Comparison Data
 Here we store other peoples result in `.csv` format, to enable automatic graph and table generation.
 * `5shot.csv`: https://arxiv.org/pdf/1909.07155.pdf
+* `6shot.csv`: https://arxiv.org/pdf/2006.02031.pdf
--- P8/P8.py
+++ P8/P8.py
@@ -3,18 +3,14 @@ from DataVisualisers.ShapeletHistogramVisualiser import ShapeletHistogramVisuali
 from DataVisualisers.ResultsVisualiser import ResultsVisualiser
 from ResultsCombiners.CSVResultsCombiner import CSVResultsCombiner
 
-experiments = ["Adiac", "Beef", "BeetleFly", "BirdChicken", "Coffee", "CricketX", "ElectricDevices", "FaceAll",
-               "FaceFour", "MedicalImages", "StrawBerry", "SwedishLeaf", "SyntheticControl", "TwoPatterns",
-               "Wine", "Yoga"]
-#experiments = ["SwedishLeaf", "Beef", "CricketX", "ElectricDevices", "Wine"]
-#experiments = ["SwedishLeaf"]
+experiments = ["ArrowHead", "BME", "CBF3", "Chinatown", "ECG200", "GunPoint", "GunPointAgeSpan", "GunPointOldVersusYoung", "ItalyPowerDemand", "MoteStrain", "Plane", "SonyAIBORobotSurface1", "SonyAIBORobotSurface2", "SyntheticControl", "ToeSegmentation1", "TwoLeadECG", "UMD", "Wine"]
 
 expSuite = ExperimentSuite(experiments, "Experiments/Configs/Base.ini", "Ours")
 ourResults = expSuite.RunExperiments()
 
 combiner = CSVResultsCombiner();
 fullResults = combiner.Combine(
-    ["../ComparisonData/5shot.csv"],
+    ["../ComparisonData/6shot.csv"],
     [ourResults],
     True
     );
