b4375b7189e6c67444df530df6ed29a9621eb1e3
Kristian Skov <kris701kj@gmail.com>
2023-04-04 23:13:41 +0200

    Added missing argument

--- P8/P8.py
+++ P8/P8.py
@@ -3,5 +3,5 @@ from ExperimentSuite import ExperimentSuite
 #experiments = ["ArrowHead", "BME", "CBF", "Chinatown", "ECG200", "GunPoint", "GunPointAgeSpan", "GunPointOldVersusYoung", "ItalyPowerDemand", "MoteStrain", "Plane", "SonyAIBORobotSurface1", "SonyAIBORobotSurface2", "SyntheticControl", "ToeSegmentation1", "TwoLeadECG", "UMD", "Wine"]
 experiments = ["ArrowHead"]
 
-expSuite = ExperimentSuite(experiments, "Experiments/Configs/Base.ini", "Ours", True)
+expSuite = ExperimentSuite(experiments, "Experiments/Configs/Base.ini", "Ours", True, False)
 ourResults = expSuite.RunExperiments()
