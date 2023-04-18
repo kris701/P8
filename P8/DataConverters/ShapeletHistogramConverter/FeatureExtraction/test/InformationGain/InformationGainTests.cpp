1309fc2b66861a974c414d92bfe7c7a8faad7f5e
Kristian Skov <kris701kj@gmail.com>
2023-04-18 10:48:06 +0200

    Attempting cmake fixes

--- P8/DataConverters/ShapeletHistogramConverter/FeatureExtraction/test/InformationGain/InformationGainTests.cpp
+++ P8/DataConverters/ShapeletHistogramConverter/FeatureExtraction/test/InformationGain/InformationGainTests.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "core/InformationGain.h"
 #include <catch2/catch_test_macros.hpp>
 #include "catch2/generators/catch_generators.hpp"
--- P8/DataConverters/ShapeletHistogramConverter/FeatureExtraction/test/window_generation/GenerateWindows.cpp
+++ P8/DataConverters/ShapeletHistogramConverter/FeatureExtraction/test/window_generation/GenerateWindows.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "core/WindowGeneration.h"
 #include <catch2/catch_test_macros.hpp>
 #include "catch2/generators/catch_generators.hpp"
